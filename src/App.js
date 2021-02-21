3f0d80177a217c89a603d3eb952401d6c9188f65
2ndTaicho <sumaiyah.domingo@gmail.com>
2021-02-21 14:59:42 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import React, { Component } from "react";
+import Map from "./containers/map";
+import "./App.css";
+
+class App extends Component {
+  render() {
+    const widthStyle = {
+      width: "800px"
+    };
+    return (
+      <div style={widthStyle}>
+        <div className="App">
+          <br />
+          <Map className="map" />
+        </div>
+        
+        <p>How to play minesweeper: (Play on Chrome)</p>
+        <p>1) The goal of the game is to NOT click any bombs</p>
+        <p>
+          2) Each number tells you how many bombs are adjacent to the "clicked"
+          cell.
+        </p>
+        <p>
+          3) The numbers range from 0 to 8. Each cell has 8 neighbors. Count
+          them.
+        </p>
+        <p>4) Use process of elimination to choose where to click next.</p>
+        <br />
+        <br />
+        <br />
+      </div>
+    );
+  }
+}
+
+export default App;
